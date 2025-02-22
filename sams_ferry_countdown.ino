0feb19c858f28356b80f1b8dee0343418b4fc028
Jakob Olason <jakobolason@gmail.com>
2025-02-22 12:06:09 +0100

    final for factorization

--- sams_ferry_countdown.ino
+++ sams_ferry_countdown.ino
@@ -19,10 +19,10 @@
 #include <ArduinoJson.h>
 
 #include "arduino_secrets.h"
+#include "timeSync.h"
 
 #define LOGGING // for debug purposes
 
-#define TIMEZONE_OFFSET_HOURS 1
 #define ORIENTATION 1 // 0 (up is where the ESP32 is), 1 (up is where the Qwiic is)
 
 unsigned long currentMillis;
@@ -41,20 +41,13 @@ char ssid[] = SECRET_SSID; // your network SSID (name)
 char pass[] = SECRET_PASS; // your network password (use for WPA, or use as key for WEP)
 char api_key[] = API_KEY;   // Unique api key to Rejseplanen API 2.0
 
-
-constexpr unsigned int LOCAL_PORT = 2390;  // local port to listen for UDP packets
-constexpr int NTP_PACKET_SIZE = 48; // NTP timestamp is in the first 48 bytes of the message
-
 int wifiStatus = WL_IDLE_STATUS;
-IPAddress timeServer(162, 159, 200, 123); // pool.ntp.org NTP server
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
 
 char baseUrl[] = "www.rejseplanen.dk/api/";
 WiFiClient client;
 int port = 443;
 
-String sælvigId = "A=1@O=Sælvig Havn (færge)@X=10549354@Y=55864255@U=86@L=110000501@B=1@p=1740046653@";
+String saelvigId = "A=1@O=Sælvig Havn (færge)@X=10549354@Y=55864255@U=86@L=110000501@B=1@p=1740046653@";
 
 ArduinoLEDMatrix matrix;
 
@@ -75,28 +68,6 @@ void rotateFrame(){
   memcpy(currentFrame, rotatedFrame, sizeof rotatedFrame);
 }
 
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address) {
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
 
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
@@ -140,59 +111,7 @@ void connectToWiFi(){
   printWifiStatus();
 }
 
-/**
- * Calculates the current unix time, that is the time in seconds since Jan 1 1970.
- * It will try to get the time from the NTP server up to `maxTries` times,
- * then convert it to Unix time and return it.
- * You can optionally specify a time zone offset in hours that can be positive or negative.
-*/
-unsigned long getUnixTime(int8_t timeZoneOffsetHours = 0, uint8_t maxTries = 5){
-  // Try up to `maxTries` times to get a timestamp from the NTP server, then give up.
-  for (size_t i = 0; i < maxTries; i++){
-    sendNTPpacket(timeServer); // send an NTP packet to a time server
-    // wait to see if a reply is available
-    delay(1000);
-
-    if (Udp.parsePacket()) {
-      Serial.println("Packet received.");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-      //the timestamp starts at byte 40 of the received packet and is four bytes,
-      //or two words, long. First, extract the two words:
-      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      
-      // Combine the four bytes (two words) into a long integer
-      // this is NTP time (seconds since Jan 1 1900):
-      unsigned long secsSince1900 = highWord << 16 | lowWord;
-
-      // Now convert NTP time into everyday time:
-      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-      const unsigned long seventyYears = 2208988800UL;
-      unsigned long secondsSince1970 = secsSince1900 - seventyYears + (timeZoneOffsetHours * 3600);
-      return secondsSince1970;
-    } else {
-      Serial.println("Packet not received. Trying again.");
-    }
-  }
-  return 0;
-}
-
-void updateTime(){
-  yield();  
-  Serial.println("\nStarting connection to NTP server...");
-  auto unixTime = getUnixTime(TIMEZONE_OFFSET_HOURS, 25);
-  Serial.print("Unix time = ");
-  Serial.println(unixTime);
-  if(unixTime == 0){
-    unixTime = getUnixTime(2,25);
-  }
-  RTCTime timeToSet = RTCTime(unixTime);
-  RTC.setTime(timeToSet);
-  Serial.println("Time updated.");
-}
-
-void searchLocation(String searchInput) {
+void searchLocation(String searchInput, String* placeId) {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("A successfull wifi connnection was not established!");
     return;
@@ -257,7 +176,7 @@ void searchLocation(String searchInput) {
         String locationId = stopLocation["id"];
         Serial.print("FOUND ID!!!: ");
         Serial.println(locationId);
-        sælvigId = locationId;
+        placeId* = locationId;
       } else {
         Serial.println("NO id found in response");
       }
@@ -272,12 +191,12 @@ void searchLocation(String searchInput) {
 void setup() {
   Serial.begin(9600);
   connectToWiFi();
-  Udp.begin(LOCAL_PORT);
+  beginUDP();
 
   RTC.begin();
   updateTime();
   matrix.begin();  
-  searchLocation("Sælvig");
+  searchLocation("Sælvig", &saelvigId);
   delay(60000);
 }
 
